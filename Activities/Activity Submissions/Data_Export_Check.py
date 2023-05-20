1f6da54bf3b8152cfb35c8922ef98550bb2e12dc
MatongoAlfred <alfred.matongo@komaza.org>
2023-05-20 14:25:07 +0300

    Kwingu Automation Test Cases

--- Activities/Activity Submissions/Data_Export_Check.py
+++ Activities/Activity Submissions/Data_Export_Check.py
@@ -67,167 +67,56 @@ activities_submission = WebDriverWait(driver, 10).until(
     ))
 activities_submission.click()
 
-# sleep for 5 seconds
+# waiting for the activities submission table to load
 time.sleep(5)
 
-# checking and clicking on the filters button
-filter_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located(
-        (By.XPATH, "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/div/button[2]"))
+# Checking if the data export icon exists
+data_export = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/div/button[3]/img"))
 )
-filter_button.click()
+data_export.click()
 
 # sleep for 3 secs
 time.sleep(3)
 
-# checking and clicking on the 'add filter' button
-add_filter = WebDriverWait(driver, 10).until(
+# Checking the export data confirmation pop up is displayed
+export_pop_up = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[1]/div/div/div/div/div/button"))
+                                    "//body/app-root/app-activity-list/app-export-grid/kendo-dialog/div[2]/kendo-dialog-titlebar/div[1]"))
 )
-add_filter.click()
+assert export_pop_up.is_displayed()
+
+# checking if the 'Cancel' button is working as expected
+cancel_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,
+                                    "//body/app-root/app-activity-list/app-export-grid/kendo-dialog/div[2]/kendo-dialog-actions/button[1]"))
+)
+cancel_button.click()
 
 # sleep for 3 secs
 time.sleep(3)
 
-# search for the 'shamba kcode' column on the popup window
-kcode_search = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "input-column"))
-)
-kcode_search.click()
-kcode_search.send_keys("shamba kcode")
-
-# sleep for 3 seconds
-time.sleep(3)
-
-# select and clicking on the shamba kcode option from the list
-wait = WebDriverWait(driver, 10)
-list_element = wait.until(EC.visibility_of_element_located((By.ID, "column-list")))
-item_xpath = "//*[@id='column-list']/li[2]/label"
-item_element = list_element.find_element(By.XPATH, item_xpath)
-item_element.click()
-
-# wait for 3 seconds
-time.sleep(3)
-
-# entering the kcode we want to filter
-filter_box_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-row-filter/div/input")
-                                   ))
-filter_box_input.click()
-filter_box_input.send_keys("kwl-2023-0212")
-
-# sleep for 3 seconds
-time.sleep(3)
-
-# checking and clicking the 'filter' button
-filter_button_two = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-row-filter/div/div/button[2]"))
-)
-filter_button_two.click()
-
-# sleep for 5 seconds
-time.sleep(5)
-
-# using internal filters to filter by status
-status_filter = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[1]/div/div/div/div[2]/div/button"))
-)
-status_filter.click()
-
-# sleep for 3 seconds
-time.sleep(3)
-
-# typing in the column we need : status column
-select_status = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[1]/div/input"))
-)
-select_status.click()
-select_status.send_keys("Status")
+# checking if the 'Save' button is available and working as expected
+data_export.click()
 
 # sleep for 3 secs
 time.sleep(3)
 
-# select and clicking on the status option from the list
-wait = WebDriverWait(driver, 10)
-list_element = wait.until(EC.visibility_of_element_located((By.ID, "column-list")))
-item_xpath = "//*[@id='column-list']/li[6]/label[1]"
-item_element = list_element.find_element(By.XPATH, item_xpath)
-item_element.click()
+# Clicking the save button
+save_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//body/app-root/app-activity-list/app-export-grid/kendo-dialog/div[2]/kendo-dialog-actions/button[2]"))
+)
+save_button.click()
 
 # sleep for 3 secs
 time.sleep(3)
 
-# searching the status we want to filter
-text_status = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-multicheck-filter/div/ul/li[1]/input"))
+# checking that the data export has begun
+export_begun = WebDriverWait(driver, 10).until(
+    EC.text_to_be_present_in_element((By.XPATH, "//body/app-root/app-activity-list/app-export-grid/kendo-dialog/div[2]/div/p"), "Preparing the CSV file for download...")
 )
-text_status.click()
-text_status.send_keys("Submitted")
-
-# sleep for 3 secs
-time.sleep(3)
-
-# clicking on the 'submitted'status
-submitted_status = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-multicheck-filter/div/ul/li[2]/input"))
-)
-submitted_status.click()
-
-# sleep for 3 secs
-time.sleep(3)
-
-# clicking on the 'Filter' button
-filter_click = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-multicheck-filter/div/div/button[2]"))
-)
-filter_click.click()
-
-# sleep for 3 secs
-time.sleep(3)
-
-# checking if the 'View Activity' icon is available and clickable
-view_activity_icon = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/div/kendo-grid-list/div[1]/div[1]/table/tbody/tr/td/span/button"))
-)
-view_activity_icon.click()
-
-# sleep for 3 secs
-time.sleep(3)
-
-# ascertain that were are in the activity full view
-activity_full_view = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located(
-        (By.XPATH, "//body/app-root/app-activity-review/div/div/div[2]/div[2]/fieldset/form/div[1]/h3"))
-)
-assert activity_full_view.is_displayed()
-
-# checking if the contract photo is available in the activity form answers
-contract_image_check = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-review/div/div/div[2]/div[2]/fieldset/form/div[2]/div/div[1]/div[29]/div/span/div[2]/button[1]"))
-)
-contract_image_check.click()
-
-# sleep for 3 seconds
-time.sleep(3)
-
-# closing the contract image
-close_image = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//body/app-root/app-activity-review/kendo-dialog/div[2]/div/span/i"))
-)
-close_image.click()
-
-# sleep for 3 seconds
-time.sleep(3)
-
-# test summary
-print("Test case executed successfully")
+# Check if the element has the expected text
+if export_begun:
+    print("Test Case passed successfully")
+else:
+    print("Test case working as expected but the text is not available")
