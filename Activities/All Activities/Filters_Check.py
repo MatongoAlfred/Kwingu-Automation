bb8b84316599fe7c3d663b27e2f9acbe43b928b5
MatongoAlfred <alfred.matongo@komaza.org>
2023-05-20 16:09:45 +0300

    Kwingu Automation Test Cases

--- Activities/All Activities/Filters_Check.py
+++ Activities/All Activities/Filters_Check.py
@@ -60,12 +60,12 @@ activities_module_select.click()
 # Sleep for 3 secs
 time.sleep(3)
 
-# select activities assignment option
-activities_assignment = WebDriverWait(driver, 10).until(
+# select all activities option
+all_activities = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located(
-        (By.XPATH, "//body/app-root/app-header/header/section/section/div/div/ul/li[5]/div[2]/li[1]/a")
+        (By.XPATH, "//body/app-root/app-header/header/section/section/div/div/ul/li[5]/div[2]/li[4]")
     ))
-activities_assignment.click()
+all_activities.click()
 
 # sleep for 5 seconds
 time.sleep(5)
@@ -79,11 +79,14 @@ close_side_menu.click()
 # sleep for 3 seconds
 time.sleep(3)
 
-# assert that were are on the activities assignment page
-activity_assignmemt_page = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "activity-ass-header"))
+# assert that were are on the all activities page
+all_activities_page = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/span"))
 )
-assert activity_assignmemt_page.is_displayed()
+assert all_activities_page.is_displayed()
+
+# sleep for 3 secs
+time.sleep(3)
 
 # look for the filter button and click on it
 filter_button = WebDriverWait(driver, 10).until(
@@ -128,7 +131,7 @@ time.sleep(3)
 # entering the kcode we want to filter
 filter_box_input = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/div/app-external-row-filter/div/input")
+                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-row-filter/div/input")
                                    ))
 filter_box_input.click()
 filter_box_input.send_keys("kwl-2023-0159")
@@ -139,7 +142,7 @@ time.sleep(3)
 # checking and clicking the 'filter' button
 filter_button_two = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH,
-                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/div/app-external-row-filter/div/div/button[2]"))
+                                    "//body/app-root/app-activity-list/div/kendo-grid/kendo-grid-toolbar/app-show-filter/div[2]/app-showfilters/div/kendo-popup/div/div/div[2]/app-external-row-filter/div/div/button[2]"))
 )
 filter_button_two.click()
 
@@ -155,7 +158,25 @@ external_filter.click()
 # sleep for 3 seconds
 time.sleep(3)
 
+# clearing the applied internal filter
+clear_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//body/app-root/kendo-popup/div/kendo-grid-filter-menu-container/form/div/app-custom-row-filter/div/div/button[1]"))
+)
+clear_button.click()
+
+# sleep for 3 secs
+time.sleep(3)
+
 # checking the external filter if it works
+external_filter1 = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//body/app-root/app-activity-list/div/kendo-grid/div/div/div[2]/table/thead/tr/th[2]/kendo-grid-filter-menu/a"))
+)
+external_filter1.click()
+
+# sleep for 3 secs
+time.sleep(3)
+
+# launch pop up here
 external_filter_input = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, "//body/app-root/kendo-popup/div/kendo-grid-filter-menu-container/form/div/app-custom-row-filter/div/input"))
 )
@@ -171,4 +192,7 @@ external_filter_button.click()
 
 # sleep for 5 seconds
 time.sleep(5)
+
+# Test summary report
+print("Test case successfully executed!!!")
 
